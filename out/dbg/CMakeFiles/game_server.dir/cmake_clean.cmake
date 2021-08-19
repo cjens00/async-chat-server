284c240620bd29d4f5fcc6efd68993756e6317cb
Supre Cam <x@x>
2021-08-19 13:22:34 -0600

    Updated directory structure.

---
+++ out/dbg/CMakeFiles/game_server.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_server.dir/main.cpp.obj"
+  "CMakeFiles/game_server.dir/net_core.cpp.obj"
+  "CMakeFiles/game_server.dir/net_core_practice.cpp.obj"
+  "game_server.exe"
+  "game_server.exe.manifest"
+  "game_server.lib"
+  "game_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
