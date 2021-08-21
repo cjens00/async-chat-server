4dbc90dd0c021de85d501efe8c671e42cab280bc
Supre Cam <x@x>
2021-08-20 21:18:58 -0600

    - Changed project name to something more appropriate.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
 #include "net_core.hpp"
-
 
 int main() {
     asio::io_context io_context;
     Server srv(io_context, 12995);
-    srv.async_accept_connection(); // Non-blocking
-    io_context.run(); // Blocking until tasks run out (never)
-
+    srv.async_accept_connection();
+    io_context.run();
     return 0;
 }
